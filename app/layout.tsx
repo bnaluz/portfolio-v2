c8ebeb30f2ca6510450f2348257ff0c748e31be3
Bobby Naluz <bnaluz9@gmail.com>
2025-09-02 00:56:28 -0400

    remove theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,10 +28,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ThemeProvider>
-          <Header />
-          {children}
-        </ThemeProvider>
+        <Header />
+        {children}
       </body>
     </html>
   );
